fbffe5d9a0e786c2ab322937532846d5feca4be2
smslsmls <skipp1223@naver.com>
2023-03-23 20:47:46 +0900

    파이썬 테트리스

---
+++ study/tetris2.py
@@ -0,0 +1,132 @@
+import time
+import os
+import ctypes
+import numpy as np
+import msvcrt
+
+background = np.array([[1,1,1,1,1,1,1,1,1,1,1,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,0,0,0,0,0,0,0,0,0,0,1],
+                       [1,1,1,1,1,1,1,1,1,1,1,1]
+                       ])
+
+block_L = np.array([[0,0,0,0],
+                    [0,1,0,0],
+                    [0,1,1,1],
+                    [0,0,0,0]])
+
+#============================================================
+
+def cls():
+    os.system('cls')
+     
+
+def gotoxy(x,y):
+    ctypes.windll.kernel32.SetConsoleCursorPosition(ctypes.windll.kernel32.GetStdHandle(-11),(((y&0xFFFF)<<0x10)|(x&0xFFFF)))
+
+def draw_background():
+    for j in range(0, 22):
+        for i in range(0,12):
+            if(background[j,i] == 1):
+                print("*",end="")
+            else:
+                print("-",end="")
+        print()
+
+def make_block():
+    for j in range(0,4):
+        for i in range(0,4):
+            if(block_L[j,i] == 1):
+                gotoxy(i+x, j+y)
+                print("*")
+            
+        
+def delete_block():
+    for j in range(0,4):
+        for i in range(0,4):
+            if(block_L[j,i] == 1):
+                gotoxy(i+x, j+y)
+                print("-")
+            #else:
+             #   gotoxy(i+x, j+y)
+              #  print("-")
+        #print()
+        
+def overlap_checka(tmp_x, tmp_y):
+    overlap_cnt = 0
+    for i in range(0, 4):
+        for j in range(0, 4):
+            if(y + tmp_y + i < 22):
+                if(block_L[i,j] == 1 & background[i+tmp_y+y,j+tmp_x+x] == 1):
+                    overlap_cnt+=1
+    return overlap_cnt
+
+def overlap_check(tmp_x, tmp_y):
+    overlap_cnt = 0
+    for i in range(0, 4):
+        for j in range(0, 4):
+            if(y + tmp_y + i < 22):
+                if(block_L[i,j] & background[i+tmp_y+y,j+tmp_x+x] == 1):
+                    overlap_cnt+=1
+    return overlap_cnt
+
+x = 3
+y = 3
+count = 0
+
+#============================================================
+
+#print(background.shape)            
+              
+draw_background()
+
+while True:
+    if msvcrt.kbhit():
+        key = msvcrt.getch()
+        
+        if key == b'a':
+            if (overlap_check(-1,0) == 0):
+                delete_block()
+                x-=1
+                make_block()
+            
+        if key == b'd':
+            if (overlap_check(1,0) == 0):
+                delete_block()
+                x+=1
+                make_block()
+            
+        if key == b's':
+            if (overlap_checka(0,1) == 0):
+                delete_block()
+                y+=1
+                make_block()
+    
+#============================================================
+    if count == 100:
+        if (overlap_checka(0,1) == 0):
+            delete_block()
+            y+=1
+            make_block()
+        count = 0
+        
+    count+=1
+    time.sleep(0.01)
